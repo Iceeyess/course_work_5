5e3cb146ae1d008cc3232bf503bc41c84f04b8c1
Dmitry Krasheninnikov <ice_eyes@mail.ru>
2024-05-07 23:50:11 +0300

    new project creation

--- course_work_5/source/funcs.py
+++ course_work_5/source/funcs.py
@@ -10,10 +10,11 @@ def get_id_employees(company_names: list[str], url: str) -> list[str]:
     res = []
     for company_name in company_names:
         params['text'] = company_name
-        response = requests.get(url, **params, **headers).text
+        response = requests.get(url, params=params, headers=headers).text
         params['text'] = ''
         res.append(response)
     return res
 
 
-print(get_id_employees(hh_company_names, employee_id_API))
+for x in get_id_employees(hh_company_names, employee_id_API):
+    print(x)
