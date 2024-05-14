370e7ed675c3db6a16e23206bb02175799a40c7a
Dmitry Krasheninnikov <ice_eyes@mail.ru>
2024-05-14 23:07:49 +0300

    DBManager class is modified.

--- database_working/main.py
+++ database_working/main.py
@@ -1,5 +1,6 @@
 from source.dbmanager import DBManager
 
 if __name__ == '__main__':
-    with DBManager() as db_conn:
-        cur = db_conn.get_companies_and_vacancies_count()
+    db_conn = DBManager()
+    with db_conn:
+        print(db_conn.get_companies_and_vacancies_count())
