315849468a688049b53a0e93d5c371bd9d27bfe9
Dmitry Krasheninnikov <ice_eyes@mail.ru>
2024-05-16 00:13:02 +0300

    DBManager class is modified. New methods were created.

--- database_working/main.py
+++ database_working/main.py
@@ -3,4 +3,4 @@ from source.dbmanager import DBManager
 if __name__ == '__main__':
     db_conn = DBManager()
     with db_conn:
-        print(db_conn.get_companies_and_vacancies_count())
+        db_conn.get_vacancies_with_keyword('Python', 'Курьер')
--- database_working/source/dbmanager.py
+++ database_working/source/dbmanager.py
@@ -11,11 +11,13 @@ class DBManager:
         self.port = port
         self.user = username
         self.password = password
+        self.params = dict(host=self.host, database=self.database, port=self.port, user=self.user,
+                           password=self.password)
 
     def __enter__(self):
         """Создает соединение для ЭК"""
         try:
-            self.conn = psycopg2.connect(**self.__dict__)
+            self.conn = psycopg2.connect(**self.params)
             return self.conn
 
         except psycopg2.OperationalError:
@@ -26,13 +28,98 @@ class DBManager:
         self.conn.commit()
         self.conn.close()
 
+    def execute_and_fetch(func):
+        """Функция-декоратор, чтобы не писать однотипный код к каждому методу.
+        в функции func возвращаются SQL запросы от всех методов класса."""
+        def wrapper(self, *args, **kwargs):
+            cur = self.conn.cursor()
+            cur.execute(func(self))
+            result = cur.fetchall()
+            [print(*_) for _ in result]
+            cur.close()
+            return result
+        return wrapper
+
+    @execute_and_fetch
     def get_companies_and_vacancies_count(self):
-        """Получает список всех компаний и количество вакансий у каждой компании."""
-        cur = self.conn.cursor()
-        cur.execute("""
+        """Печатает и возвращает список всех компаний и количество вакансий у каждой компании."""
+        return """
         SELECT e.name_, COUNT(v.vacancy_id) as quantity_vacancies
         FROM vacancies v
         INNER JOIN employers e USING(vacancy_id)
         GROUP BY e.name_;
-        """)
-        return cur.fetchall()
+        """
+
+    @execute_and_fetch
+    def get_all_vacancies(self):
+        """Печатает и возвращает список всех вакансий с указанием названия компании, названия вакансии и зарплаты и
+        ссылки на вакансию."""
+        return """
+        SELECT e.name_ AS entity_name, v.name_ AS vacancy_name, s.from_ AS salary_from, s.to_ AS salary_to, v.alternate_url as url
+        FROM vacancies v
+        INNER JOIN salaries s USING(vacancy_id)
+        INNER JOIN employers e USING(vacancy_id);
+        """
+
+    @execute_and_fetch
+    def get_avg_salary(self):
+        """Печатает и возвращает среднюю зарплату по вакансиям,
+        в которых есть данные отличные от нуля, если в обоих колонках from_/to_ нулевые значения, то не учитываем
+        строку, если только в from_ значение отличное от нуля, то учитываем только его, если значение отличное от нуля
+        только в to_, то _from не учитываем, т.к. там ноль. Примеры, 0.00 - 100000 = 100000, 100000 - 200000 = 100000,
+        100000 - 0.00 = 100000. Учитываем только такой формат"""
+        return """
+        SELECT AVG(avarage)
+        FROM(
+        SELECT CASE WHEN from_ <> 0 THEN from_ ELSE to_ END AS avarage 
+        FROM(SELECT from_, to_
+        FROM salaries
+        WHERE from_ <> 0 OR to_ <> 0))
+        """
+
+    @execute_and_fetch
+    def get_vacancies_with_higher_salary(self):
+        """
+        Функция возвращает список вакансий, зарплата которых выше средней заработной платы
+        всех вакансий таблицы vacancies
+        """
+        return """
+        SELECT v.name_
+        FROM vacancies v
+        INNER JOIN salaries s USING(vacancy_id)
+        WHERE 
+	        (s.from_ <> 0 AND s.from_ > (SELECT AVG(avarage)
+		        FROM(SELECT CASE WHEN from_ <> 0 THEN from_ ELSE to_ END AS avarage 
+			        FROM(SELECT from_, to_
+				        FROM salaries
+				        WHERE from_ <> 0 OR to_ <> 0))))
+        OR 
+	        (s.to_ <> 0 AND s.to_ > (SELECT AVG(avarage)
+		        FROM(SELECT CASE WHEN from_ <> 0 THEN from_ ELSE to_ END AS avarage 
+			        FROM(SELECT from_, to_
+				        FROM salaries
+				        WHERE from_ <> 0 OR to_ <> 0))));
+        """
+
+    @execute_and_fetch
+    def get_vacancies_with_keyword(self, *args, **kwargs):
+        """Возвращает список всех вакансий, в названии которых присутствуют переданные в функцию слово
+        или список слов"""
+        self.lst = [_ for _ in args]
+        if len(self.lst) > 1:
+            for _ in args[1:]:
+                result = f"""
+            SELECT *
+            FROM vacancies
+            WHERE name_ like '%{args[0]}%'
+            """ + f"""OR name_ like '%{_}%'"""
+            return self.result + ';'
+        elif len(self.lst) == 1:
+            return f"""
+            SELECT *
+            FROM vacancies
+            WHERE name_ like '%{args[0]}%'
+            """
+        else:
+            print(f"Неверно введены данные в функцию get_vacancies_with_keyword")
+
